f80957c72dab72bf00260f78eb3de811cab7d504
Sinan Nar <Sinan.Nar@eroad.com>
2024-07-26 00:16:28 +1200

    adds open telemetry console exporter

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,9 +1,14 @@
+using OpenTelemetry.Logs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddOpenTelemetry(x => x.AddConsoleExporter());
 
 var app = builder.Build();
 
